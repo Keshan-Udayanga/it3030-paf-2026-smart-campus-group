ba896f8576c13948ac03fa2f42802fc9dd32e69e
matheeshagit <12matheesha@gmail.com>
2026-03-08 09:58:28 +0530

    Java files for Role, User and UserRepository created

---
+++ back_end/src/main/java/smart_campus/back_end/auth/model/Role.java
@@ -0,0 +1,7 @@
+package smart_campus.back_end.auth.model;
+
+public class Role {
+    public static final String USER = "USER";
+    public static final String ADMIN = "ADMIN";
+    public static final String TECHNICIAN = "TECHNICIAN";
+}
