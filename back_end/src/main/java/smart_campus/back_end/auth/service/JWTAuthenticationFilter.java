cfbbf1d55c7bb90eb8084b43207991f168612e0e
matheeshagit <12matheesha@gmail.com>
2026-03-20 10:10:56 +0530

    Implement JWTAuthenticationFilter to check jwt token in every request

--- back_end/src/main/java/smart_campus/back_end/auth/service/JWTAuthenticationFilter.java
+++ back_end/src/main/java/smart_campus/back_end/auth/service/JWTAuthenticationFilter.java
@@ -1,4 +1,47 @@
 package smart_campus.back_end.auth.service;
 
-public class JWTAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import smart_campus.back_end.auth.model.User;
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        String email = jwtService.extractEmail(token);
+
+        User user = userService.findByEmail(email);
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(user, null, customUserDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
 }
