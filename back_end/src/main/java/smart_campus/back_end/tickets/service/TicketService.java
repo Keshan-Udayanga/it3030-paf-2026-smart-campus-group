b1de51b079c6681c3719bf1fd1b51ad9d38a292f
matheeshagit <12matheesha@gmail.com>
2026-04-22 10:15:03 +0530

    Revert "feat(module-c): implement create ticket service and controller"
    This reverts commit 16b74b1d5ae93bf32a4456ead9d75674a91a2170.
    "Changes made"

--- back_end/src/main/java/smart_campus/back_end/tickets/service/TicketService.java
+++
@@ -1,9 +0,0 @@
-package smart_campus.back_end.tickets.service;
-
-import smart_campus.back_end.tickets.dto.TicketRequestDTO;
-import smart_campus.back_end.tickets.entity.Ticket;
-
-public interface TicketService {
-
-    Ticket createTicket(TicketRequestDTO ticketRequestDTO);
-}
